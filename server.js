eb45df2c2d0169dcbb98834cbcc25587fda86a08
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-10-08 15:49:41 -0400

    added server parser for text and json.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var app = express();
+var parser = require("body-parser");
 var PORT = 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+app.use(parser.json({ type: }));
+app.use(parser.text());
+
 
 app.use(express.static("public"));
 
