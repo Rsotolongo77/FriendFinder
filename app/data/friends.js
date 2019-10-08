0b02b60122b051252254a15a5a8c4a4c9c8fc80e
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-10-08 16:49:54 -0400

    added profiles to friends js.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 var friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
+        name: "Caviar Lonwich",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
             5,
             1,
             4,
@@ -16,19 +16,147 @@ var friends = [
         ]
     },
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores": [
+        name: "Mary Richardsonbonnivilledanglewitzcrivera",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            1,
+            3,
+            4,
+            2,
             5,
+            1,
+            5,
+            2,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Bill Smitty",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            3,
+            2,
+            5,
+            5,
+            1,
+            3,
             1,
             4,
             4,
+            3
+        ]
+    },
+    {
+        name: "Unluthy Sante",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            3,
+            2,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Patty Sanders",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            3,
+            3,
+            2,
+            2,
+            5,
             5,
             1,
             2,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Daniel Boon",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            5,
+            2,
+            3,
+            4,
+            2,
+            3,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Jason Tart",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            4,
+            3,
+            1,
+            1,
+            1,
+            1,
+            3,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Bosman Dongothere",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            3,
+            2,
+            1,
+            4,
+            4,
             5,
             4,
-            1
+            2
+        ]
+    },
+    {
+        name: "Jessie Pope",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            2,
+            4,
+            3,
+            3,
+            4,
+            5,
+            3,
+            2,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            2,
+            3,
+            2,
+            2,
+            3,
+            5,
+            1,
+            4,
+            5,
+            5
         ]
     }
 ];
