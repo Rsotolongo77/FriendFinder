1ebc57473efc73189c14f1f1ad48e835e9aed8bb
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-10-09 16:32:58 -0400

    added css to head of HTML due to not rendering from seperate css specific file.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,13 +3,30 @@ var friends = require("../data/friends");
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.JSON(friends)
+        console.log(friends);
     });
 
     app.post("/api/friends", function (req, res) {
-        //logic that compares scores
-        //2 for loops, user1 and user2 
-        //subtract each score and get the difference from each score question
-        //POST after scores calculated 
+        //send all information here and do calculations
+        var userData = req.body;
+        var scores = userData.scores;
+        totalScore = [];
 
-    });
-}
+        //console.log(userData.scores);
+
+        for (let i = 0; i < scores.length; i++) {
+            totalScore.push(parseInt(scores[i]));
+        }
+
+        //console.log(totalScore);
+        for (let j = 0; j < friends.length; j++) {
+            //console.log(friends[j].name, friends[j].scores)
+            var totalDifference = totalScore.map(
+                function (a, i) {
+                    return Math.abs(a - parseInt(friends[j].scores[i]));
+                });
+            console.log(`${friends[j].name}'s total diff from you is is ${totalDifference.reduce((a, b) => a + b, 0)}`);
+        }
+    })
+
+};
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require("express");
 var app = express();
 var parser = require("body-parser");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+var path = require("path");
 
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
-app.use(parser.json({ type: }));
-app.use(parser.text());
+//app.use(parser.json({ type: "application/vnd.api+json" }));
+//app.use(parser.text());
 
 
 app.use(express.static("public"));
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function () {
-    console.log("Port at : " + PORT);
+    console.log("Server listening on: http://localhost:" + PORT);
 })
