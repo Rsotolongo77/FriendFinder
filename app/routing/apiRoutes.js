fdae79d0a305c300d7df4f9a3d2b6399945aee5f
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-10-10 08:49:16 -0400

    added some notes.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,43 +1,46 @@
-var friends = require('../data/friends.js');
+//pulls in data from friends.js (to be used in algorithm below).
+var friends = require("../data/friends.js");
 
+//makes apiRoutes a module with data available to server.js for routing.
 module.exports = function (app) {
-    app.get('/api/friends', function (req, res) {
+    //get request established to grab data from friends.js for processing in function below. Returned as JSON.
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-
-    app.post('/api/friends', function (req, res) {
+    //post request established to display/load result from function below to route - api/friends.
+    app.post("/api/friends", function (req, res) {
+        //make large initial value for comparision; and establish empty variables for dynamic filling at end of function.
         var difference = 100;
-        var matchName = '';
-        var matchPhoto = '';
-
+        var mName = "";
+        var mPhoto = "";
+        //run loop funtion for each oject in array. Friend established as placeholder/property to be given final value at end of function.
         friends.forEach(function (friend) {
-
-            var matchedScoresArray = [];
-            var totalDifference = 100;
-
+            //empty scores arry established to asign number after for-loop below. Then to be used for comparison to difference variable. 
+            var scoresArray = [];
+            //helps set score of user and friends.js database objects into a single value. 
             function add(total, num) {
                 return total + num;
             }
-
+            //loop comparing user score with friends.js/objects/scores and putting results in scores array
             for (var i = 0; i < friend.scores.length; i++) {
-                matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
+                scoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
 
             }
-
-            totalDifference = matchedScoresArray.reduce(add, 0);
-
+            //reduces the array results into a single value with assistance of add function established above
+            totalDifference = scoresArray.reduce(add, 0);
+            //checks totaldifference to previous difference to then asign matching object with lowest difference
             if (totalDifference < difference) {
                 difference = totalDifference;
-                matchName = friend.name;
-                matchPhoto = friend.photo;
+                mName = friend.name;
+                mPhoto = friend.photo;
             }
         });
-
+        //result returned as JSON 
         res.json({
-            name: matchName,
-            photo: matchPhoto
+            name: mName,
+            photo: mPhoto
         });
-
+        //Result posted to body of page.
         friends.push(req.body);
     });
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,18 @@
 var path = require('path');
 
+//declares htmlRoutes as a module and makes it available to other files via require. 
 module.exports = function (app) {
 
+    //displays home page via express
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
 
     });
-
+    ///displays survey page via express
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
+    //redirects non-existing route to home page
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
 
